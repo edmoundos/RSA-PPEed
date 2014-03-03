004a78dc13a94844376a8cffa9ca72b30c7066eb
clercc <clercc@uw.edu>
2014-03-03 20:52:44 +0100

    update 3.3.2014 CLERC
    ajouté une boucle et une fonction

--- fusion_a_cc.py
+++ fusion_a_cc.py
@@ -1,25 +1,49 @@
+from datetime import datetime
+from openfisca_core import model
+import openfisca_france
+openfisca_france.init_country()
+from openfisca_core.simulations import ScenarioSimulation
 # PPE Modifiee
 def fusion_a(div_ms, ra_rsa, br_rmi_i, br_rmi_ms, br_rmi_pf, br_rmi, rmi_nbp, forf_log, rsa_socle, rmi, rsa, rsa_act, rsa_act_i, crds_mini, ppe_coef, ppe_base, ppe_coef_tp, ppe_elig, ppe_elig_i, ppe_rev, ppe_brute, ppe):
-    """
-    J'ajoute tous les variables qui composent le RSA et la PPE pour pouvoir
-    créer des booleans et des boucles si nous en avons besoin ainsi que changer
-    les bases des composants de la PPE et du RSA sans changer les fichiers.
-    """ 
-#     Pour calculer le rmi/RSA des le premier euro
-    rmi = max_(1, rsa_socle - forf_log - br_rmi)
-    return rmi
-
-    P = _P.minim.rmi
-    RSA = max_(1, rsa_socle + P.pente * (ra_rsa[CHEF] + ra_rsa[PART]) - forf_log - br_rmi)
-    rsa = RSA * (RSA >= 12 * P.rsa_nv)
-    return rsa
     
-    res = max_(rsa - rmi, 1)
-    return res
+    for agent in QUIFOY:
+        if sal >=1 & sal <= 0.7*SMIC:
+            total = min(sal, 0.7 * SMIC)
+            fusion = min(12*total, 2000)
+
+        elif sal >0.7 * SMIC & sal <=1.1 * SMIC:
+            total = max((77/40)*SMIC - (7/4)*sal, 0)
+            fusion = min(12*total, 2000)
+      
+        else:
+            fusion = 0
+        break
+
+def case_study(year = 2013):
+    
+    simulation = ScenarioSimulation()
+    simulation.set_config(year = year,
+                          nmen = 20, 
+                          maxrev = 20000,
+                          x_axis = 'sali')
+    scenario = simulation.scenario
+    simulation.set_param() #legislation#
+    scenario = simulation.scenario
+    simulation.set_param()
+    df = simulation.get_results_dataframe(index_by_code=True)
+
+    print df.info()
+    print df.to_string()
+
+
+if __name__ == '__main__':
+    case_study(fusion_a)
+
+          
 
 #     TODO: - Ajoute la PPE
-#           - Changer l'age de 25 à 18 ans
-#           - Créer une pente jusqu'à 0,7 du SMIC et décroit jusqu'au point de sortie
+#           - Changer l'age de 25 a 18 ans
+#           - Creer une pente jusqu'a 0,7 du SMIC et decroit jusqu'au point de sortie
 #           - Changer le point de sortie (1,1 du SMIC)
 #           
 #
